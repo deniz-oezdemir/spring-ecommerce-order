c60ef9d61ba070fdc688649a1e8255a1785f04c3
Deniz Oezdemir <oezdemir.de@gmail.com>
2025-08-14 10:53:41 +0200

    refactor(config): convert StripeProperties to a regular class - Convert the StripeProperties from a `data class` to a regular `class`

--- src/main/kotlin/ecommerce/config/StripeProperties.kt
+++ src/main/kotlin/ecommerce/config/StripeProperties.kt
@@ -3,6 +3,6 @@ package ecommerce.config
 import org.springframework.boot.context.properties.ConfigurationProperties
 
 @ConfigurationProperties("stripe")
-data class StripeProperties(
+class StripeProperties(
     val secretKey: String,
 )
